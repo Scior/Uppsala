cbf0972e3e8d0f646e6173571951303f71c13a8b
Scior <interval12@gmail.com>
2018-12-31 18:55:24 +0900

    Initial commit

---
+++ Uppsala/Uppsala.h
@@ -0,0 +1,19 @@
+//
+//  Uppsala.h
+//  Uppsala
+//
+//  Created by Suita Fujino on 2018/12/31.
+//  Copyright © 2018 Suita Fujino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Uppsala.
+FOUNDATION_EXPORT double UppsalaVersionNumber;
+
+//! Project version string for Uppsala.
+FOUNDATION_EXPORT const unsigned char UppsalaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Uppsala/PublicHeader.h>
+
+
